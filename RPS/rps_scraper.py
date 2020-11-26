3a981affb9587091e94b0f6cc6a1c9295b1462f2
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-11-26 19:27:54 +0000

    initial structure setup

--- RPS/rps_scraper.py
+++ RPS/rps_scraper.py
@@ -9,3 +9,28 @@ import datetime
 class RPS:
     def __init__(self):
         print("RPS")
+
+    def checkTableExists(self, dbcon, tablename):
+        dbcur = dbcon.cursor()
+        dbcur.execute("""
+            SELECT COUNT(*)
+            FROM information_schema.tables
+            WHERE table_name = '{0}'
+            """.format(tablename.replace('\'', '\'\'')))
+        if dbcur.fetchone()[0] == 1:
+            dbcur.close()
+            return True
+
+        dbcur.close()
+        return False
+
+    def push_to_DB(self):
+        pass
+
+    # TODO
+    def get_data(self):
+        pass
+
+    # TODO
+    def run(self):
+        pass
--- UCL_DISCOVERY/discovery_scraper.py
+++ UCL_DISCOVERY/discovery_scraper.py
@@ -8,3 +8,28 @@ import datetime
 class UCL_Discovery:
     def __init__(self):
         print("Discovery")
+
+    def checkTableExists(self, dbcon, tablename):
+        dbcur = dbcon.cursor()
+        dbcur.execute("""
+            SELECT COUNT(*)
+            FROM information_schema.tables
+            WHERE table_name = '{0}'
+            """.format(tablename.replace('\'', '\'\'')))
+        if dbcur.fetchone()[0] == 1:
+            dbcur.close()
+            return True
+
+        dbcur.close()
+        return False
+
+    def push_to_DB(self):
+        pass
+
+    # TODO
+    def get_data(self):
+        pass
+
+    # TODO
+    def run(self):
+        pass
