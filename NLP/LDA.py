ffd573cdf20db2c0fcd33e67f8d5db6672204546
Kareem Kermad <krimado@gmail.com>
2021-03-04 03:03:55 +0000

    add tSNE clustering visualisation to LDA.

--- NLP/LDA.py
+++ NLP/LDA.py
@@ -1,5 +1,6 @@
 import gensim
 import numpy as np
+import pandas as pd
 import nltk
 import json
 import logging
@@ -11,31 +12,28 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from preprocess import module_catalogue_tokenizer, text_lemmatizer, get_stopwords
 from ModelResults.resultsProcessing import ProcessResults
 
+import matplotlib.colors as mcolors
+from sklearn.manifold import TSNE
+from bokeh.plotting import figure, output_file, save
+from bokeh.models import Label
+
 class LDA():
     def __init__(self, data, keywords):
-        # module-catalogue data frame with columns {ModuleID, Description}.
-        self.data = data
-        self.keywords = keywords  # topic keywords list.
+        self.data = data # module-catalogue data frame with columns {ModuleID, Description}.
+        self.keywords = keywords # list of topic keywords.
         self.vectorizer = self.create_vectorizer(1, 3, 1, 0.03)
-        #self.vectorizer = self.create_vectorizer(1, 1, 1, 1)
         self.n_topics = len(self.keywords)
 
     def create_vectorizer(self, min_n_gram, max_n_gram, min_df, max_df):
-        # lemmatize stopwords.
-        stopwords = [text_lemmatizer(s) for s in get_stopwords()]
-        return TfidfVectorizer(tokenizer=module_catalogue_tokenizer, stop_words=stopwords, ngram_range=(min_n_gram, max_n_gram),
-                               strip_accents='unicode', min_df=min_df, max_df=max_df)
-        # return CountVectorizer(tokenizer=module_catalogue_tokenizer, stop_words=stopwords, ngram_range=(min_n_gram, max_n_gram),
-        #     strip_accents='unicode', min_df=min_df, max_df=max_df)
+        stopwords = [text_lemmatizer(s) for s in get_stopwords()] # lemmatize stopwords.
+        return TfidfVectorizer(tokenizer=module_catalogue_tokenizer, stop_words=stopwords, ngram_range=(min_n_gram, max_n_gram), strip_accents='unicode', 
+                min_df=min_df, max_df=max_df)
 
-    # 1e4
     # optimized alpha [0.07905018, 0.021635817, 0.02065896, 0.021123013, 0.022771074, 0.26609096, 0.017549414, 0.02452033, 0.02282285, 0.03086126, 0.023378693, 0.02263801, 0.021001419, 0.017342038, 0.023954913, 0.024656, 0.019133672, 0.024959238]
 
-    def create_model(self, corpus, id2word, eta, n_passes, n_iterations, alpha):
-        #return gensim.models.LdaModel(corpus=corpus, num_topics=self.n_topics, id2word=id2word, random_state=42, chunksize=5000, eta=eta,
-        #update_every=1, passes=n_passes, alpha='auto', minimum_probability=0, per_word_topics=True)
+    def create_model(self, corpus, id2word, eta, n_passes, n_iterations):
         return gensim.models.LdaMulticore(corpus=corpus, num_topics=self.n_topics, id2word=id2word, random_state=42, chunksize=5000, eta=eta,
-                                          eval_every=None, passes=n_passes, iterations=n_iterations, workers=3, alpha="symmetric", minimum_probability=0, per_word_topics=True)
+                eval_every=None, passes=n_passes, iterations=n_iterations, workers=3, alpha="symmetric", minimum_probability=0, per_word_topics=True)
 
     def create_topic_seeds(self):
         tf_feature_names = self.vectorizer.get_feature_names() # list of words or ngrams of words.
@@ -62,7 +60,7 @@ class LDA():
     def train(self, n_passes, n_iterations, num_top_words):
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-        X = self.vectorizer.fit_transform(self.data.Description) # maps description column to matrix of documents (rows) and counts (columns).
+        X = self.vectorizer.fit_transform(self.data.Description) # map description column to a matrix with documents as rows and counts as columns.
         corpus = gensim.matutils.Sparse2Corpus(X, documents_columns=False)
         id2word = dict((v, k) for k, v in self.vectorizer.vocabulary_.items())
 
@@ -72,18 +70,16 @@ class LDA():
         with (np.errstate(divide='ignore')):
             self.model = self.create_model(corpus, id2word, eta, n_passes, n_iterations)
         
-        # Display perplexity, topic-word and document-topic distributions.
+        # Display perplexity, topic-word distribution and document-topic distribution.
         self.display_perplexity(corpus)
         print()
         self.display_topic_words(num_top_words)
         print()
         self.display_document_topics(corpus)
 
-        # Record current results
-        self.writeResults(corpus, num_top_words)
+        self.writeResults(corpus, num_top_words) # record current results.
 
-        # Process results, stored in ModelResults directory
-        self.processResults(threshold=15)
+        self.processResults(threshold=15) # process results stored in the ModelResults directory.
 
         # Visualize model using pyLDAvis.
         # self.visualize_model(corpus)
@@ -96,7 +92,7 @@ class LDA():
             print('SDG {}: {}'.format(n + 1, [self.model.id2word[w] for w, p in self.model.get_topic_terms(n, topn=num_top_words)]))
 
     def display_document_topics(self, corpus):
-        documents = self.data.Module_ID #self.data.Description
+        documents = self.data.Module_ID
         count = 0
         for d, c in zip(documents, corpus):
             if count % 25 == 0:
@@ -104,7 +100,7 @@ class LDA():
                 print('{} {}'.format(d, doc_topics))
             count = count + 1
 
-    def visualize_model(self, corpus):
+    def py_lda_vis(self, corpus):
         d = corpora.Dictionary()
         word2id = dict((k, v) for k, v in self.vectorizer.vocabulary_.items())
         d.id2token = self.model.id2word
@@ -125,10 +121,34 @@ class LDA():
                                           for w, p in self.model.get_topic_terms(n, topn=num_top_words)]
 
         data['Document Topics'] = {}
-        documents = self.data.Module_ID  # self.data.Description
+        documents = self.data.Module_ID
         for d, c in zip(documents, corpus):
             doc_topics = ['({}, {:.1%})'.format(topic + 1, pr) for topic, pr in self.model.get_document_topics(c)]
             data['Document Topics'][d] = doc_topics
 
         with open('ModelResults/training_results.json', 'w') as outfile:
             json.dump(data, outfile)
+
+    def tSNE_cluster(self, corpus):
+        # Find topic weights.
+        topic_weights = []
+        for i, row_list in enumerate(self.model[corpus]):
+            topic_weights.append([w for i, w in row_list[0]])
+
+        df = pd.DataFrame(topic_weights).fillna(0).values # topic weights dataframe.
+        df = df[np.amax(df, axis=1) > 0.35]
+
+        topic = np.argmax(df, axis=1) # highest related topic (SDG).
+
+        # t-SNE Dimensionality Reduction.
+        tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=.99, init='pca')
+        tsne_lda = tsne_model.fit_transform(df)
+
+        # Plot topic clusters.
+        colors = np.array([color for name, color in mcolors.TABLEAU_COLORS.items()])
+        plot = figure(title="t-SNE Clustering of {} SDGs".format(self.n_topics), 
+              plot_width=2000, plot_height=1500)
+
+        plot.scatter(x=tsne_lda[:,0], y=tsne_lda[:,1], color=colors[topic])
+        output_file("t_sne_clusters.html")
+        save(plot)
