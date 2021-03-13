39efacdb596f454327bb63c1763550c5e17ae2a6
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2021-03-13 04:02:28 +0000

    datafram from pubLoader done

--- LOADERS/publication_loader.py
+++ LOADERS/publication_loader.py
@@ -1,11 +1,24 @@
 import pymongo
 import json
 from LOADERS.loader import Loader
+import pandas as pd
 
 class PublicationLoader(Loader):
-    def load(self, count):
-        client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.hw8fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-        db = client.Scopus
-        col = db.Data
-        data = col.find()
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.hw8fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        self.db = client.Scopus
+        self.col = db.Data
+
+    def load(self):
+        data = self.col.find()
         return data
+
+    def load(self, count):
+        data = self.col.find().limit(count)
+        df = pd.DataFrame(columns=["Publication Name", "DOI", "Abstract"])
+
+        for i in data:
+            rowDf = pd.DataFrame([[i["Title"], i["DOI"], i["Abstract"]]], columns=df.columns)
+            df = df.append(rowDf)
+        
+        return df
