9166b4b0d2a0a0f15cdccd788bb531624f3493f9
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2021-03-27 21:51:44 +0000

    libraries uploaded

---
+++ src/test/test_scopus.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_scopus_api(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    # def test_sum_tuple(self):
+    #     self.assertEqual(sum((1, 2, 2)), 5, "Should be 6")
