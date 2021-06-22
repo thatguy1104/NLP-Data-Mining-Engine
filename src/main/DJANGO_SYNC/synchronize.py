584882fcd25bbd4e50e3d6faed7b41b3d56805b9
Aashvin <55795472+Aashvin@users.noreply.github.com>
2021-06-22 14:59:00 +0100

    fixed the update query for publications

--- src/main/DJANGO_SYNC/synchronize.py
+++ src/main/DJANGO_SYNC/synchronize.py
@@ -159,16 +159,19 @@ class Synchronizer():
     def __update_postgres_data(self, data_sdg:dict, title:str) -> None:
         con = psycopg2.connect(database='summermiemiepostgre', user='miemie_admin@summermiemie', host='summermiemie.postgres.database.azure.com', password='e_Paswrd?!', port='5432')
         cur = con.cursor()
-        print(str(data_sdg))
-        test_string = str(data_sdg)
 
         #Replace some instances of the single quotes in the JSON file to 2* single quotes so it can be parsed by PostgreSQL
+        query_string = str(data_sdg).replace("{'", "{''").replace("': '", "'': ''").replace("', '", "'', ''").replace("': {", "'': {").replace("Similarity'", "Similarity''").replace("'SDG_Keyword_Counts'", "''SDG_Keyword_Counts''").replace("'ColorRed'", "''ColorRed''").replace("'ColorGreen'", "''ColorGreen''").replace("'ColorBlue'", "''ColorBlue''").replace("'StringCount'", "''StringCount''").replace("'ModelResult'", "''ModelResult'").replace("''IHE'", "''IHE''").replace("'IHE_Prediction'", "''IHE_Prediction'").replace("''SVM'", "''SVM''").replace("'SVM_Prediction'", "''SVM_Prediction'").replace("'''}", "''''}")
+        query_string = json.dumps(query_string)
+        for x in range(1, 19):
+            replace_string = "('" + str(x) + "'"
+            updated_string = "(" + str(x)
+            query_string = query_string.replace(replace_string, updated_string)
+        
         cur.execute(
-            'UPDATE public.app_publication SET \"assignedSDG\" = \'{}\' WHERE title = \'{}\''.format(json.loads(json.dumps("\"" + str(data_sdg).replace("{'", "{''").replace("': '", "'': ''").replace("', '", "'', ''").replace("': {", "'': {").replace("Similarity'", "Similarity''").replace("'SDG_Keyword_Counts'", "''SDG_Keyword_Counts''").replace("'ColorRed'", "''ColorRed''").replace("'ColorGreen'", "''ColorGreen''").replace("'ColorBlue'", "''ColorBlue''").replace("'StringCount'", "''StringCount''").replace("'ModelResult'", "''ModelResult'").replace("''IHE'", "''IHE''").replace("'IHE_Prediction'", "''IHE_Prediction'").replace("''SVM'", "''SVM''").replace("'SVM_Prediction'", "''SVM_Prediction'").replace("'''}", "''''}") + "\"")), title)
+            'UPDATE public.app_publication SET \"assignedSDG\" = \'{}\' WHERE title = \'{}\''.format(json.loads(json.dumps(query_string)), title)
         )
-
         con.commit()
-
         cur.close()
 
     def __loadSDG_Data_PUBLICATION(self) -> None:
@@ -208,16 +211,13 @@ class Synchronizer():
                 if len(postgre_publication) < 4:
                     self.__create_column_postgres_table()
 
-                # with open('oke.json', 'w') as outfile:
-                #     json.dump(publication_SDG_assignments, outfile)
-                #     return
-                # self.__update_postgres_data(publication_SDG_assignments, data_[i]['Title'])
+                self.__update_postgres_data(publication_SDG_assignments, data_[i]['Title'])
 
-            # print(i['Title'])
-            # self.__update_postgres_data(publication_SDG_assignments, data_[i]['Title'])
+            print(data_[i]['Title'])
+
 
     def run(self):
-        # self.__loadSDG_Data_PUBLICATION()
+        self.__loadSDG_Data_PUBLICATION()
         t = "Outcomes following surgery in subgroups of comatose and very elderly patients with chronic subdural hematoma"
         publ = self.__getPostgres_modules(title=t)
         print(len(publ))
