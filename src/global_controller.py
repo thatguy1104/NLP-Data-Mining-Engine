e24399b9f66e0c3c784bafb44a1c1ea4d86bc22f
Aashvin <55795472+Aashvin@users.noreply.github.com>
2021-07-13 13:15:32 +0100

    re-added sync manager section to global manager

--- src/global_controller.py
+++ src/global_controller.py
@@ -3,6 +3,7 @@ from main.MANAGERS.loader_manager import LOADER_SECTION
 from main.MANAGERS.module_manager import MODULE_SECTION
 from main.MANAGERS.scopus_manager import SCOPUS_SECTION
 from main.MANAGERS.nlp_manager import NLP_SECTION
+from main.MANAGERS.synchronizer_manager import SYNC_SECTION
 
 def keywords_merger_manager(sdg_keywords: bool) -> None:
     keywords_merger_actions = KEYWORDS_MERGER_SECTION() 
